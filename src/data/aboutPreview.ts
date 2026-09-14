8be16b19385d8c665548ace57f6e439198ab1ae0
Sakib548 <mdsakibhassan548@gmail.com>
2026-09-14 21:49:55 +0600

    s

--- src/data/aboutPreview.ts
+++ src/data/aboutPreview.ts
@@ -3,5 +3,5 @@ export const aboutPreview = {
   title: "Creating Value Across Industries",
   description:
     "SAMPAN Group has evolved into one of Bangladesh's diversified business groups, building successful ventures across real estate, education, construction, economic zones, retail, agriculture, automotive, and leisure.",
-  href: "/about",
+  href: "/about ",
 };
